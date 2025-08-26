797e0e61745764395fa083fcfc758d128ead8b40
MKP715 <mkp715@gmail.com>
2025-08-26 02:17:41 -0500

    Delete extract_text.py

--- extract_text.py
+++
@@ -1,8 +0,0 @@
-import PyPDF2
-
-with open('bills_story.pdf', 'rb') as f:
-    reader = PyPDF2.PdfReader(f)
-    text = ""
-    for page in reader.pages:
-        text += page.extract_text()
-    print(text)
